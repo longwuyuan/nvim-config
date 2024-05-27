f6b8259bb11110f75d8cc19924fe6e805caee406
longwuyuan <longwuyuan@gmail.com>
2024-05-27 06:37:19 +0530

    disabled the autoenabled inlay hints

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,14 @@
+-- inlay hints disable
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      inlay_hints = {
+        enabled = false,
+      },
+      -- document_highlight = {
+      --   enabled = false,
+      -- },
+    },
+  },
+}
