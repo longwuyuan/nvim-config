1e9199e1e903ca253e073146a3abb2f1e847a46f
longwuyuan <longwuyuan@gmail.com>
2024-12-03 01:41:16 +0530

    init

---
+++ lua/plugins/cmp-emoji.lua
@@ -0,0 +1,8 @@
+-- add cmp-emoji
+return {
+  "hrsh7th/nvim-cmp",
+  dependencies = { "hrsh7th/cmp-emoji" },
+  opts = function(_, opts)
+    table.insert(opts.sources, { name = "emoji" })
+  end,
+}
---
+++ lua/plugins/gruvbox.lua
@@ -0,0 +1,21 @@
+return {
+  -- add gruvbox
+  {
+    "ellisonleao/gruvbox.nvim",
+    config = function()
+      require("gruvbox").setup({
+        contrast = "hard",
+        palette_overrides = {
+          dark0_hard = "#000000",
+        },
+      })
+    end,
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+}
